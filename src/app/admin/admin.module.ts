38e0f5f80cef16a8d526c446951dcdfff1594a9d
Trung Nghĩa Phạm <trungnghiamap>
2019-03-14 16:08:12 +0700

    router admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,28 @@ import { CommonModule } from '@angular/common';
 import { QuanLyNguoiDungComponent } from './quan-ly-nguoi-dung/quan-ly-nguoi-dung.component';
 import { QuanLyPhimComponent } from './quan-ly-phim/quan-ly-phim.component';
 import { QuanLyLichChieuComponent } from './quan-ly-lich-chieu/quan-ly-lich-chieu.component';
+import { LayoutAdminComponent } from './layout-admin/layout-admin.component';
+
+import { RouterModule, Routes } from '@angular/router'
+
+const adminRoutes :Routes= [
+  {path:'admin', component:LayoutAdminComponent},
+  {path:'ql-nguoi-dung', component: QuanLyNguoiDungComponent},
+  {path:'ql-phim', component: QuanLyPhimComponent},
+  {path:'ql-lich-chieu', component: QuanLyLichChieuComponent}
+]
 
 @NgModule({
-  declarations: [QuanLyNguoiDungComponent, QuanLyPhimComponent, QuanLyLichChieuComponent],
+  declarations: [QuanLyNguoiDungComponent,
+    QuanLyPhimComponent,
+    QuanLyLichChieuComponent,
+    LayoutAdminComponent],
   imports: [
-    CommonModule
+    RouterModule.forRoot(adminRoutes),
+    CommonModule,
+  ],
+  exports: [
+    LayoutAdminComponent
   ]
 })
 export class AdminModule { }
